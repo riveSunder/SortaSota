4d293ea1824c78b0d1ea9b20199cc0d5d9fe555a
RiveSunder <rivesunder@protonmail.com>
2022-01-16 17:57:31 -0500

    implement adjacency matrix and graph-to-grid function
    note you need a 5x5 grid for the glider to work in Life!

--- life_like_graphs/life_like_graphs.py
+++ life_like_graphs/life_like_graphs.py
@@ -2,6 +2,7 @@ import argparse
 import unittest
 
 import numpy as np
+#import matplotlib.pyplot as plt
 
 gaussian = lambda x: np.exp(- (x**2 / 0.5**2) / 2)  
 
@@ -29,56 +30,113 @@ def get_ca_mlp(birth=[3], survival=[2,3]):
         return out 
 
     return mlp 
+
+def ca_graph(length):
+    
+    # nodes
+    # number of nodes is the side of the grid, squared.
+    num_nodes = length**2
+    nodes = np.zeros((num_nodes, 1))
+    node_indices = np.arange(num_nodes)
+
+    # edges
+    num_edges = 8 * num_nodes
+    edges = np.zeros((num_edges, 1))
+    
+    # senders & receivers
+    senders = np.vstack(\
+            [node_indices - length -1, \
+            node_indices - length, \
+            node_indices - length + 1, \
+            node_indices - 1, \
+            node_indices + 1, \
+            node_indices + length - 1, \
+            node_indices + length, \
+            node_indices + length + 1])
+    sender = senders.T.reshape(-1)
+
+    senders = (senders + length**2) % length**2
+    receivers = np.repeat(node_indices, 8)
+
+    return (num_nodes, num_edges, nodes, edges, senders, receivers)
+
+def add_glider(graph_tuple):
+
+    nodes = graph_tuple[2]
+    length = int(np.sqrt(graph_tuple[0]))
+
+    nodes[0, 0] = 1.0
+    nodes[1, 0] = 1.0  
+    nodes[2, 0] = 1.0
+    nodes[2 + length, 0] = 1.0
+    nodes[1 + 2 * length, 0] = 1.0
+
+    return (graph_tuple[0], nodes, graph_tuple[2], \
+            graph_tuple[3], graph_tuple[4], graph_tuple[5])
+
+
+def get_adjacency(graph_tuple):
+
+    num_nodes = graph_tuple[0]
+    num_edges = graph_tuple[1]
+    length = int(np.sqrt(graph_tuple[0]))
+    
+    senders = graph_tuple[4]
+    receivers = graph_tuple[5]
+
+    adjacency_matrix = np.zeros((num_nodes, num_nodes))
+    
+    for xx in range(receivers.shape[0]):
+
+        for yy in senders[:, receivers[xx]]:
+            adjacency_matrix[receivers[xx], yy] = 1.0
+
+    return adjacency_matrix
+
+def get_graph_grid(graph_tuple):
+
+    length = int(np.sqrt(graph_tuple[0]))
+
+    grid = np.zeros((length, length))
+
+    
+    for ii, node_state in enumerate(graph_tuple[2]):
+
+        grid[ ii // length, ii % length] = node_state
+
+    return grid
+
+
         
-class TestGetCAMLP(unittest.TestCase):
-
-    def setup(self):
-        
-        pass
-
-    def test_life(self):
-
-        b = [3]
-        s = [2,3]
-
-        life_mlp = get_ca_mlp(b, s)
-
-        self.assertEqual(life_mlp(2. + 9), 1.0)
-        self.assertEqual(life_mlp(3. + 9), 1.0)
-        self.assertEqual(life_mlp(3.), 1.0)
-
-        self.assertEqual(life_mlp(np.array([[2. + 9]])), 1.0)
-        self.assertEqual(life_mlp(np.array([[3. + 9]])), 1.0)
-        self.assertEqual(life_mlp(np.array([[3.]])), 1.0)
-
-        self.assertNotEqual(life_mlp(4. + 9), 1.0)
-        self.assertNotEqual(life_mlp(1. + 9), 1.0)
-        self.assertNotEqual(life_mlp(2.), 1.0)
-        self.assertNotEqual(life_mlp(4.), 1.0)
-
-    def test_general(self):
-
-        for run in range(10):
-            # nb - number off birth rules to use
-            nb = np.random.randint(9)
-            ns = np.random.randint(9)
-            survival = np.random.choice(np.arange(9), size=(ns,), replace=False)
-            birth = np.random.choice(np.arange(9), size=(nb,), replace=False)
-
-            ca_mlp = get_ca_mlp(birth, survival)
-
-            for bb in np.arange(9):
-                self.assertEqual(ca_mlp(bb), 1.0 * bb in birth) 
-                self.assertEqual(ca_mlp(np.array([[bb]])), 1.0 * bb in birth) 
-
-            for ss in np.arange(9):
-                self.assertEqual(ca_mlp(ss + 9), 1.0 * ss in survival) 
-                self.assertEqual(ca_mlp(np.array([[ss + 9]])), \
-                        1.0 * ss in survival) 
-
-
-
 if __name__ == "__main__":
 
-    unittest.main(verbosity = 2)
+    graph_tuple = ca_graph(5)   
+
+    grid = get_graph_grid(graph_tuple)
+
+    graph_tuple = add_glider(graph_tuple)
+    grid = get_graph_grid(graph_tuple)
+
+    a_matrix = get_adjacency(graph_tuple)
+
+    print(grid) 
+    mlp = get_ca_mlp()
+
+
+    gt = graph_tuple
+    for ii in range(10):
+        
+        new_nodes = gt[2]
+
+        
+        new_nodes = mlp(((a_matrix @ new_nodes) + 9 * new_nodes).T).T
+
+        gt = (gt[0], gt[1], new_nodes, gt[3], gt[4], gt[5])
+
+        grid = get_graph_grid(gt)
+        print(grid)
+        
+        import pdb; pdb.set_trace()
+
+
 
